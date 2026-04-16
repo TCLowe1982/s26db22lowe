094d2e56cf1519cd21801123b41ca3e758cf3bdc
TC Lowe <s272122@nwmissouri.edu>
2026-04-15 21:46:06 -0500

    Create Books
    Added create book functionality

--- controllers/book.js
+++ controllers/book.js
@@ -106,9 +106,12 @@ exports.book_view_one_Page = async function(req, res) {
 };
 
 // Handle building the view for creating a book.
+// No body, no in path parameter, no query.
+// Does not need to be async
 exports.book_create_Page =  function(req, res) {
+    console.log("create view")
     try{
-        res.render('bookcreate', { title: 'Create New Book' });
+        res.render('bookcreate', { title: 'Book Create'});
     }
     catch(err){
         res.status(500)
